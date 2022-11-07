f8e27025e6d3cbf04b61442ac179889602ffa683
TomiLuca <tomasngomez1@gmail.com>
2022-11-07 12:10:32 -0300

    feat: Ultimos arreglos

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -1,60 +1,34 @@
-import "./Styles/ItemList.css"
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
-import {useEffect, useState} from 'react'
-import {data} from '../utils/data'
-import {customFetch} from '../utils/customFetch'
-import Item from "./Item"
-
+import {useState} from 'react'
+import data from '../utils/data'
 
 
 const ItemList = () => {
+  const [rate,setRate] = useState(0);
+  const stockMaximo = 10;
+  const stockMinimo = 0;
 
-  const [datos,setDatos] = useState([])
+  const rateVideo = () => {
+    if(rate >= stockMaximo){
+    alert("No hay mas stock")
+    } else{
+      setRate(rate + 1)
+    }
+  }
 
-  //componentDidMount
-  useEffect(() => {
-    customFetch(2000,data)
-      .then(response => setDatos(response))
-      .catch(err => console.log(err))
-  },[])
-
-  // const [rate,setRate] = useState(0);
-  // const stockMaximo = 10;
-  // const stockMinimo = 0;
-
-  // const rateVideo = () => {
-  //   if(rate >= stockMaximo){
-  //   alert("No hay mas stock")
-  //   } else{
-  //     setRate(rate + 1)
-  //   }
-  // }
-
-  // const restVideo = () => {
-  //   if(rate <= stockMinimo){
-  //    alert("Error,seleccione al menos 1 prenda") 
-  //   }else{
-  //     setRate(rate - 1)
-  //   }
-  // }
+  const restVideo = () => {
+    if(rate <= stockMinimo){
+     alert("Error,seleccione al menos 1 prenda") 
+    }else{
+      setRate(rate - 1)
+    }
+  }
 
   return (
     <>
-     { 
-      datos.map(item => (
-        <ItemList
-        // thumbnails={buzo_beige_frente}
-        title={item.title}
-        genero={item.genero}
-        material={item.material}
-        talles={item.talles}
-        Img={Item.Img}
-        />
-      ))
-    }
 
-    {/* // <Card style={{ width: '18rem', float:'left' , margin:'50px',justifycontent:'center', alingitems:"center"}}>
+    // <Card style={{ width: '18rem', float:'left' , margin:'50px',justifycontent:'center', alingitems:"center"}}>
     //   <Card.Img variant="top" src={props.Img}/>
     //   <Card.Body>
     //     <Card.Title><h4>{props.title}</h4></Card.Title>
@@ -68,7 +42,7 @@ const ItemList = () => {
     //     <Button variant="outline-dark" onClick={rateVideo}>+</Button> | <Button variant="outline-dark" onClick={restVideo}>-</Button>
     //     <Button variant="info" >Agregar al carrito</Button>
     //   </Card.Body>
-    // </Card> */}
+    // </Card>
     </>
   );
 }
--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -1,16 +1,40 @@
-import Item from "./ItemList"
 import ItemCount from "./ItemCount" 
+import {useEffect, useState} from 'react'
+import {data} from '../utils/data'
+import {customFetch} from '../utils/customFetch'
 import ItemList from "./ItemList"
 
 const ItemListContainer = (props) => {
+    const [datos,setDatos] = useState([])
+
+    //componentDidMount
+    useEffect(() => {
+        customFetch(2000,data)
+        .then(response => setDatos(response))
+        .catch(err => console.log(err))
+    },[])
+    
     return(
-        <>
-        <div>
-        <h1><b><i>{props.greeting}</i></b></h1>
-        </div>
-        <ItemList/>
-        <ItemCount />
-        </>
+    <>
+    <div>
+    <h1><b><i>{props.greeting}</i></b></h1>
+    </div>
+
+    { 
+      datos.map(item => (
+        <ItemList
+        // thumbnails={buzo_beige_frente}
+        title={item.title}
+        genero={item.genero}
+        material={item.material}
+        talles={item.talles}
+        Img={Item.Img}
+        />
+      ))
+    }
+
+    <ItemCount />
+    </>
     )
 }
 
