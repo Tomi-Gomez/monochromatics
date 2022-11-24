1c7f3c76f74cf2e2db984e480adc0e3521084f5f
TomiLuca <tomasngomez1@gmail.com>
2022-11-24 16:02:43 -0300

    feat: Context y Rendering

--- src/Components/CardWidget.js
+++ src/Components/CardWidget.js
@@ -12,7 +12,7 @@ const CardWidget = () => {
         <Link to="/cart"><button type="button" class="btn btn-primary position-relative">
             <img className="img_carrito" src={Pic}></img>
             <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-              {test.calcularStock}
+              {test.calcularStock()}
               <span class="visually-hidden">unread messages</span>
             </span><i class="bi bi-cart4"></i>
           </button>
