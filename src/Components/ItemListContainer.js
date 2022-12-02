3f17af2b01f609ef737794acb0fbdb9d090d62c5
TomiLuca <tomasngomez1@gmail.com>
2022-12-01 22:25:04 -0300

    Entrega Proyecto-Final/GomezTomas

--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -12,37 +12,29 @@ const ItemListContainer = (props) => {
 
    //componentDidUpdate
    useEffect(() => {
-    // creamos una función que va a obtener los datos de firebase
+    
     const getData = async () => {
-    // con una condicional, si no tiene categorías,
+    
     const queryRef = !IdCategory
-    // va a traer todos los productos
     ? collection(db, "productos")
-    // si tiene categorías, firevase va a filtrarlas
     : query(
     collection(db, "productos"),
     where("category", "==", parseInt(IdCategory))
     );
-    // recibimos los datos
+ 
     const response = await getDocs(queryRef);
-    // y hacemos un map para crear objetos con esos datos.
     const productos = response.docs.map((docs) => {
     const newProduct = {
     ...docs.data(),
     id: docs.id,
     };
-    // lo retornamos
     return newProduct;
     });
     setTimeout(() => {
-    // simulamos una demora de 2' y actualizamos el estado datos con los productos
-    // ya sea con los que filtramos o con todos.
     setDatos(productos);
     }, 2000)
     };
-    // llamamos a la función
     getData();
-    
     }, [IdCategory])
 
 
